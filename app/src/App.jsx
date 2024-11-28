d3a1213a2f016c2fc4892258028d0d39401d3c39
erYashSurywanshi <yashhard0@gmail.com>
2024-11-28 15:40:13 +0530

    There is some data is added by me

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,163 +2,149 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import SearchResult from "./components/SearchResults/SearchResult";
 
-
-export const BASE_Link ="http://localhost:9000"
-
+export const BASE_Link = "http://localhost:9000";
 
 const App = () => {
+  const [data, setdata] = useState(null);
+  const [error, setError] = useState(null);
+  const [loeding, setLoeding] = useState(false);
+  const [filter, setfilter] = useState(null);
+  const [selectedbtn, setselectedbtn] = useState("all");
+  useEffect(() => {
+    const Api = async () => {
+      setLoeding(true);
+      try {
+        const responce = await fetch(BASE_Link);
 
+        const json = await responce.json();
 
+        setdata(json);
+        setfilter(json);
+        setLoeding(false);
+      } catch (error) {
+        setError("Unable to fetch data");
+      }
+    };
+    Api();
+  }, []);
 
-const [data, setdata]= useState(null)
-const [error,setError] =useState(null)
-const [loeding,setLoeding] = useState(false)
-const [filter,setfilter] = useState(null)
-const [selectedbtn,setselectedbtn] = useState("all")
-useEffect(()=>{
+  const filtersearch = (e) => {
+    const filtervalue = e.target.value;
 
-  const Api = async()=>{
-
-    setLoeding(true);
-  try{
-    const responce = await fetch(BASE_Link)
-
-  const json = await responce.json();
-
- 
-   setdata(json)
-   setfilter(json)
-    setLoeding(false)
-  } catch(error){
-      setError("Unable to fetch data")
-  }
-} 
-Api()
-},[])
-
-const filtersearch =(e)=>{
-      const filtervalue =e.target.value
-     
-
-    if(filtersearch ==''){
-      filtersearch(null)
+    if (filtersearch == "") {
+      filtersearch(null);
     }
 
-    const filteredFood = data?.filter((food)=>
-   food.name.toLowerCase().includes(filtervalue.toLowerCase())
-    )
-    setfilter(filteredFood)
-};
+    const filteredFood = data?.filter((food) =>
+      food.name.toLowerCase().includes(filtervalue.toLowerCase())
+    );
+    setfilter(filteredFood);
+  };
 
- const filterfood =(type)=>{
-   if(type == "all"){
-     setfilter(data)
-     return;
-   } else {
-     const filteredFood = data?.filter((food)=>
-     food.type.toLowerCase() == type.toLowerCase()
-     )
-     setfilter(filteredFood)
-     setselectedbtn(type)
-   }
- }
+  const filterfood = (type) => {
+    if (type == "all") {
+      setfilter(data);
+      return;
+    } else {
+      const filteredFood = data?.filter(
+        (food) => food.type.toLowerCase() == type.toLowerCase()
+      );
+      setfilter(filteredFood);
+      setselectedbtn(type);
+    }
+  };
 
-  const btnclick =[
+  const btnclick = [
     {
       name: "all",
-      type: "all"
+      type: "all",
     },
     {
       name: "lunch",
-      type: "lunch"
+      type: "lunch",
     },
     {
       name: "breakfast",
-      type: "breakfast"
+      type: "breakfast",
     },
     {
       name: "dinner",
-      type: "dinner"
-    }
-  ]
-if (error) return <div>{error}</div>
-if (loeding) return <div>Loading....</div>
- 
-  return( 
-  
+      type: "dinner",
+    },
+  ];
+  if (error) return <div>{error}</div>;
+  if (loeding) return <div>Loading....</div>;
+
+  return (
     <Container>
       <Topcontainer>
         <div>
           <img src="./logo.svg" alt="" />
         </div>
         <div>
-          <input onChange={filtersearch} type="text" placeholder="Search Food...."/>
+          <input
+            onChange={filtersearch}
+            type="text"
+            placeholder="Search Food...."
+          />
         </div>
       </Topcontainer>
-         <ButtonContainer>
-          {btnclick.map((value)=>
-          <Button key={value.type} onClick={()=> filterfood(value.type)}>{value.name}</Button>
-          )}
-          
-          
-         </ButtonContainer>
-         <SearchResult data={filter} />
+      <ButtonContainer>
+        {btnclick.map((value) => (
+          <Button key={value.type} onClick={() => filterfood(value.type)}>
+            {value.name}
+          </Button>
+        ))}
+      </ButtonContainer>
+      <SearchResult data={filter} />
     </Container>
-    
-  )
+  );
 };
 
 export default App;
 
 const Container = styled.div`
-  
   max-height: 1200px;
   margin: 0 auto;
-`
+`;
 const Topcontainer = styled.div`
-
   display: flex;
   height: 140px;
   justify-content: space-between;
   align-items: center;
   padding: 25px 120px;
-  
 
-div input{
-  width: 285px;
-  padding: 5px;
-  border-radius: 5px;
-  border: 1px solid #ff0000;
-  background-color: transparent;
-  color: white;
-  border-bottom: 2px solid red;
-  background-color: black;
+  div input {
+    width: 285px;
+    padding: 5px;
+    border-radius: 5px;
+    border: 1px solid #ff0000;
+    background-color: transparent;
+    color: white;
+    border-bottom: 2px solid red;
+    background-color: black;
+
+    @media (0 < width < 600px) {
+      width: 180px;
+    }
+  }
 
   @media (0 < width < 600px) {
-    width: 180px;
-  }
-}
-
-@media (0 < width < 600px){
     flex-direction: column;
     height: 120px;
-}
-`
+  }
+`;
 const ButtonContainer = styled.div`
-
   display: flex;
   gap: 12px;
   justify-content: center;
   padding-bottom: 40px;
-  
-`
+`;
 export const Button = styled.button`
-
   padding: 5px 10px;
   border-radius: 5px;
   border: none;
   background-color: red;
   color: white;
   cursor: pointer;
-`
-
+`;
--- server/dist/index.js
+++ server/dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
@@ -10,56 +12,129 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)());
-app.use("/images", express_1.default.static(path_1.default.join(__dirname, "../public/images")));
+app.use(
+  "/images",
+  express_1.default.static(path_1.default.join(__dirname, "../public/images"))
+);
 app.get("/", (req, res) => {
-    console.log(path_1.default.join(__dirname, "../public"));
-    const foodData = [
-        {
-            name: "Boilded Egg",
-            price: 10,
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            image: "/images/egg.png",
-            type: "breakfast",
-        },
-        {
-            name: "RAMEN",
-            price: 25,
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            image: "/images/ramen.png",
-            type: "lunch",
-        },
-        {
-            name: "GRILLED CHICKEN",
-            price: 45,
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            image: "/images/chicken.png",
-            type: "dinner",
-        },
-        {
-            name: "CAKE",
-            price: 18,
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            image: "/images/cake.png",
-            type: "breakfast",
-        },
-        {
-            name: "BURGER",
-            price: 23,
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            image: "/images/burger.png",
-            type: "lunch",
-        },
-        {
-            name: "PANCAKE",
-            price: 25,
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            image: "/images/pancake.png",
-            type: "dinner",
-        },
-    ];
-    res.json(foodData);
+  console.log(path_1.default.join(__dirname, "../public"));
+  const foodData = [
+    {
+      name: "Boilded Egg",
+      price: 10,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/egg.png",
+      type: "breakfast",
+    },
+    {
+      name: "RAMEN",
+      price: 25,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/ramen.png",
+      type: "lunch",
+    },
+    {
+      name: "GRILLED CHICKEN",
+      price: 45,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/chicken.png",
+      type: "dinner",
+    },
+    {
+      name: "CAKE",
+      price: 18,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/cake.png",
+      type: "breakfast",
+    },
+    {
+      name: "BURGER",
+      price: 23,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/burger.png",
+      type: "lunch",
+    },
+    {
+      name: "PANCAKE",
+      price: 25,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/pancake.png",
+      type: "dinner",
+    },
+    {
+      name: "Ramen",
+      type: "Lunch",
+      text: "Savory noodle soup with a rich broth, fresh vegetables, and soft-boiled eggs.",
+      price: 25.00,
+      image: "https://example.com/images/ramen.jpg",
+    },
+    {
+      name: "Sushi Platter",
+      type: "Lunch",
+      text: "Delicious assortment of fresh sushi rolls and sashimi.",
+      price: 35.00,
+      image: "https://example.com/images/sushi.jpg",
+    },
+    {
+      name: "Pasta Primavera",
+      type: "Dinner",
+      text: "Fresh vegetables tossed with penne pasta in a light cream sauce.",
+      price: 18.00,
+      image: "https://example.com/images/pasta.jpg",
+    },
+    {
+      name: "Cheeseburger",
+      type: "Lunch",
+      text: "Juicy beef patty topped with melted cheese and served with fries.",
+      price: 12.00,
+      image: "https://example.com/images/cheeseburger.jpg",
+    },
+    {
+      name: "Caesar Salad",
+      type: "Lunch",
+      text: "Crisp romaine lettuce with creamy Caesar dressing and croutons.",
+      price: 10.00,
+      image: "https://example.com/images/caesar-salad.jpg",
+    },
+    {
+      name: "Grilled Salmon",
+      type: "Dinner",
+      text: "Tender grilled salmon served with a lemon butter sauce.",
+      price: 30.00,
+      image: "https://example.com/images/grilled-salmon.jpg",
+    },
+    {
+      name: "Chicken Curry",
+      type: "Dinner",
+      text: "Spicy and flavorful chicken curry served with steamed rice.",
+      price: 20.00,
+      image: "https://example.com/images/chicken-curry.jpg",
+    },
+    {
+      name: "Vegetable Stir-fry",
+      type: "Lunch",
+      text: "Mixed vegetables stir-fried with soy sauce and sesame seeds.",
+      price: 15.00,
+      image: "https://example.com/images/veg-stirfry.jpg",
+    },
+    {
+      name: "Margherita Pizza",
+      type: "Dinner",
+      text: "Classic pizza with tomato, mozzarella, and fresh basil.",
+      price: 22.00,
+      image: "https://example.com/images/margherita-pizza.jpg",
+    },
+    {
+      name: "Chocolate Lava Cake",
+      type: "Breakfast",
+      text: "Decadent chocolate cake with a molten center.",
+      price: 8.00,
+      image: "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
+    },
+  ];
+  res.json(foodData);
 });
 app.listen(9000, () => {
-    console.log("Server is running on port 9000");
+  console.log("Server is running on port 9000");
 });
 //# sourceMappingURL=index.js.map
